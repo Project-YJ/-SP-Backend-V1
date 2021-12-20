e2146df2f09911460ed12ca7cfc024f212dd661f
이준서 <ljchan71@gmail.com>
2021-12-20 17:16:14 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/main/java/com/example/spbackendv1/SpBackendV1Application.java
@@ -0,0 +1,13 @@
+package com.example.spbackendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBackendV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpBackendV1Application.class, args);
+    }
+
+}
